f635f90a06f722624ddfa59eda6938afabda212d
FawwazAF <fawwazaf123@gmail.com>
2021-08-08 23:28:55 +0700

    first commit

---
+++ connect-to-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i D:/Documents/Alterra/AWS/aws_ec2.pem ubuntu@3.23.129.87
---
+++ copy-to-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -r -i D:/Documents/Alterra/AWS/aws_ec2.pem ./program/* ubuntu@3.23.129.87:/home/ubuntu/app
